605cbd435c254ff147f923a5c77d1ce06a534400
JorgeegroJ13 <139525015+JorgeegroJ13@users.noreply.github.com>
2023-07-13 23:53:20 -0400

    Add files via upload
    acabo de subir archivos

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConceptosBasicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = 20;
+            Console.WriteLine("hola mundo!"+year);
+            Console.ReadLine();
+        }
+    }
+}
